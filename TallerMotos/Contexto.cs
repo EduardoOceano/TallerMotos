5a8b54fa34204d0a56e7dd42d1028ba448584bac
Eduardo <eduardochiscuet@fp.oceanoatlantico.org>
2021-04-20 09:39:42 +0200

    Contexto, string de conexion, accesorios y navbar

---
+++ TallerMotos/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace TallerMotos.Models
+{
+    public class Contexto: DbContext
+    {
+        public Contexto([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Accesorios> Accesorios { get; set; }
+    }
+}
---
+++ TallerMotos/Models/Accesorios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerMotos.Models
+{
+    public class Accesorios
+    {
+        int idAccesorio { get; set; }
+        string tipo { get; set; }
+        string fabricante { get; set; }
+        decimal precio { get; set; }
+
+    }
+}
