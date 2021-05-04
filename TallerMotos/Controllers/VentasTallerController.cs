4c9fbba0c2d475c331490dd8ba9d5b7419280f36
Jose Luis Gardeta <prutote@gmail.com>
2021-05-04 13:03:29 +0200

    Signed-off-by: Jose Luis Gardeta <prutote@gmail.com>

---
+++ TallerMotos/Controllers/VentasTallerController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+using TallerMotos.Models;
+using TallerMotos.Models.ViewData;
+
+namespace TallerMotos.Controllers
+{
+    public class VentasTallerController : Controller
+    {
+        private readonly Contexto _context;
+
+        public VentasTallerController(Contexto context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<VentasTaller> lista = new List<VentasTaller>();
+            using (DbCommand cn = _context.Database.GetDbConnection().CreateCommand())
+            {
+                cn.CommandText = "SELECT SUM(f.total), t.idTaller, f.fecha FROM facturas f INNER JOIN talleres t ON(t.idTaller = f.idTaller) GROUP BY YEAR(f.fecha) ORDER BY t.idTaller";
+                _context.Database.OpenConnection();
+                using (DbDataReader dr = cn.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        VentasTaller ventas = new VentasTaller();
+                        ventas.total = Decimal.Parse(dr["total"].ToString());
+                        ventas.idTaller = int.Parse(dr["idTaller"].ToString());
+                        ventas.fecha = (DateTime)dr["fecha"];
+                       
+                        lista.Add(ventas);
+                    }
+                }
+
+            }
+            return View(lista);
+        }
+    }
+}
---
+++ TallerMotos/Models/ViewData/VentasTaller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerMotos.Models.ViewData
+{
+    public class VentasTaller
+    {
+        public decimal total { get; set; }
+        public int idTaller { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
