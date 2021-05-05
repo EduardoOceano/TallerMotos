c0a80a3d1fe9a4b64790fd864b34250a5c8870b8
Jose Luis Gardeta <prutote@gmail.com>
2021-05-05 14:34:32 +0200

    Otro listao mas ko
    Signed-off-by: Jose Luis Gardeta <prutote@gmail.com>

---
+++ TallerMotos/Models/ViewData/ClientesFacturas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerMotos.Models.ViewData
+{
+    public class ClientesFacturas
+    {
+        public Decimal total { get; set; }
+        public int idCliente { get; set; }
+        public string nombreCliente { get; set; }
+    }
+}
