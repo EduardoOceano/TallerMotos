ae2888fd76f1403acaaff622d595cc6b2cd04a9b
estelagotapuertolas@fp.oceanoatlantico.org <estelagotapuertolas@fp.oceanoatlantico.org>
2021-05-05 13:54:22 +0200

    tengo que terminar esto :c

---
+++ TallerMotos/Models/ViewData/FacturasTaller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerMotos.Models.ViewData
+{
+    public class FacturasTaller
+    {
+        public int idFactura { get; set; }
+        public string nombreCliente { get; set; }
+        public int idTaller { get; set; }
+        public string ciudad { get; set; }
+    }
+}
