b46734b5984fe5802b44f1009631feff9f378c51
Daniel <danielespadagargallo@fp.oceanoatlantico.org>
2021-05-07 08:09:48 +0000

    Push ClientesTaller

---
+++ TallerMotos/Models/ViewData/ClientesTalleres.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerMotos.Models.ViewData
+{
+    public class ClientesTalleres
+    {
+        public string nombreCliente { get; set; }
+        public string apellidoCliente { get; set; }
+        public int idTaller { get; set; }
+        public string ciudad { get; set; }
+    }
+}
