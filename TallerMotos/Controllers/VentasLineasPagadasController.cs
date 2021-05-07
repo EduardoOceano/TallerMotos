274a2bf8341efc7133ceed7e10cf335eab72bf47
estelagotapuertolas@fp.oceanoatlantico.org <estelagotapuertolas@fp.oceanoatlantico.org>
2021-05-07 12:47:13 +0200

    un desto

---
+++ TallerMotos/Controllers/VentasLineasPagadasController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TallerMotos.Models;
+using TallerMotos.Models.ViewData;
+
+namespace TallerMotos.Controllers
+{
+    public class VentasLineasPagadasController : Controller
+    {
+        private readonly Contexto _context;
+        private readonly ServicioSQL _sql;
+
+        public VentasLineasPagadasController(Contexto context, ServicioSQL sql)
+        {
+            _context = context;
+            _sql = sql;
+        }
+
+        public IActionResult Index(string factura)
+        {
+            string sql = " SELECT cantidad, precio, idFactura, isPagado FROM ventasLineas " +
+                " WHERE isPagado= 1 " + 
+                ( factura != null && factura!= "" ? " AND idFactura = @idFactura" : "");
+
+            MySqlParameter[] par =
+            {
+                new MySqlParameter("@idFactura", factura)
+            };
+
+
+            List<VentasLineasPagadas> lista = _sql.EjecutarSQL<VentasLineasPagadas>(
+                   _context,
+                   sql,
+                   x => new VentasLineasPagadas()
+                   {
+                       cantidad = x.GetInt32(0),
+                       precio = x.GetDecimal(1),
+                       idFactura = x.GetInt32(2),
+                       isPagado = x.GetBoolean(3)
+                   },
+                   par
+               );
+
+            ViewBag.VLPagada = new SelectList(_context.VentasLineas, "FacturasId", "FacturasId", factura);
+
+            return View(lista);
+        }
+    }
+}
---
+++ TallerMotos/Models/ViewData/VentasLineasPagadas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TallerMotos.Models.ViewData
+{
+    public class VentasLineasPagadas
+    {
+        public int idVentasLineas { get; set; }
+        public int idProducto { get; set; }
+        public int idServicio { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+        public int idFactura { get; set; }
+
+        public bool isPagado { get; set; }
+
+    }
+}
